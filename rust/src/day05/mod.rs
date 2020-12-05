21f2fda9c5788561c1c9c65338252cc5de420f04
Ivan Smirnov <i.s.smirnov@gmail.com>
2020-12-05 23:12:53 +0000

    Day 05: Rust (part 2 - abysmal XOR-based solution)

--- rust/src/day05/mod.rs
+++ rust/src/day05/mod.rs
@@ -81,7 +81,68 @@ pub fn part1(s: &[u8]) -> u16 {
     unsafe { unmangle(r) }
 }
 
+#[inline]
+fn xor_cum2(v1: u8, v2: u8, n: usize, k: usize, i: usize) -> u8 {
+    // byte values `v1` and `v2`; total of `n` bytes; `k`-th combination; byte number `i`
+    /*
+    Cumulative XORs have the following interesting pattern (with 'L' and 'R' as an example):
+        [76 76 76 76] -> [76 76 76 76]
+        [76 76 76 82] -> [ 0  0  0 30]
+        [76 76 82 76] -> [76 76 82 82]
+        [76 76 82 82] -> [0 0 0 0]
+        [76 82 76 76] -> [76 82 76 76]
+        [76 82 76 82] -> [ 0  0  0 30]
+        [76 82 82 76] -> [76 82 82 82]
+        [76 82 82 82] -> [0 0 0 0]
+        [82 76 76 76] -> [82 76 76 76]
+        [82 76 76 82] -> [ 0  0  0 30]
+        [82 76 82 76] -> [82 76 82 82]
+        [82 76 82 82] -> [0 0 0 0]
+        [82 82 76 76] -> [82 82 76 76]
+        [82 82 76 82] -> [ 0  0  0 30]
+        [82 82 82 76] -> [82 82 82 82]
+        [82 82 82 82] -> [0 0 0 0]
+     This can be quite easily formalized into a somewhat abysmally looking formula...
+     */
+    if i == n - 1 {
+        [v1, v1 ^ v2, v2, 0][k % 4]
+    } else {
+        if k % 2 == 1 {
+            0
+        } else {
+            let m = k >> 1;
+            let p = n - 1 - i;
+            let q = (m % (1 << p)) >> (p - 1);
+            [v1, v2][q]
+        }
+    }
+}
+
+#[inline]
+fn xor_cum_last3(k: usize) -> u8 {
+    // [220, 221, 222, 223, 236, 237, 246, 247] xored indefinitely repeats every 8
+    [220, 1, 223, 0, 236, 1, 247, 0][k % 8]
+}
+
+#[inline]
+fn xor_cum(k: usize) -> u64 {
+    let mut a: [u8; 8] = [0; 8];
+    for i in 0..6 {
+        a[i] = xor_cum2(b'F', b'B', 10, k, i);
+    }
+    a[7] = xor_cum_last3(k) ^ 0xff;
+    return u64::from_be_bytes(a);
+}
+
 pub fn part2(s: &[u8]) -> u16 {
-    // TODO
-    0
+    let (a, b, x) =
+        s.chunks(11)
+            .take(s.len() / 11)
+            .fold((u64::MIN, u64::MAX, 0), |(a, b, x), s| {
+                let m = unsafe { mangle(s) };
+                (a.max(m), b.min(m), x ^ m)
+            });
+    let (a, b) = unsafe { (unmangle(a), unmangle(b)) };
+    let (xa, xb) = (xor_cum((a - 1) as _), xor_cum(b as _));
+    unsafe { unmangle(xa ^ xb ^ x) }
 }
