2b75dbe7e2c1e009444425c682f59ae8ed9480b6
Ivan Smirnov <i.s.smirnov@gmail.com>
2020-12-06 15:15:28 +0000

    Day 06: Rust (get rid of all subtractions)

--- rust/src/day06/mod.rs
+++ rust/src/day06/mod.rs
@@ -7,13 +7,13 @@ pub fn input() -> &'static [u8] {
 pub fn part1(s: &[u8]) -> u16 {
     let mut prev = b'\n';
     let mut group = 0u32;
-    let mut sum = 0;
+    let mut sum = 0u16;
     for &c in s {
-        let c = c.wrapping_sub(b'\n');
+        let c = c ^ b'\n';
         if c != 0 {
-            group |= 1u32 << c.wrapping_sub(b'a' - b'\n');
+            group |= 1u32 << (c & 0x1f)
         } else if prev == 0 {
-            sum += group.count_ones() as u16;
+            sum = sum.wrapping_add(group.count_ones() as u16);
             group = 0;
         }
         prev = c;
@@ -25,16 +25,16 @@ pub fn part2(s: &[u8]) -> u16 {
     let mut prev = b' ';
     let mut person = 0;
     let mut group = u32::MAX;
-    let mut sum = 0;
+    let mut sum = 0u16;
     for &c in s {
-        let c = c.wrapping_sub(b'\n');
+        let c = c ^ b'\n';
         if c != 0 {
-            person |= 1u32 << c.wrapping_sub(b'a' - b'\n');
+            person |= 1u32 << (c & 0x1f);
         } else if prev != 0 {
             group &= person;
             person = 0;
         } else {
-            sum += group.count_ones() as u16;
+            sum = sum.wrapping_add(group.count_ones() as _);
             person = 0;
             group = u32::MAX;
         }
