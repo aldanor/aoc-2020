038d7c4f7a4cb0940e4ba50b4cfc3a806d7ed140
Ivan Smirnov <i.s.smirnov@gmail.com>
2020-12-08 21:45:28 +0000

    Day 04: Rust - cleanup and slight speedup

--- rust/src/day04/mod.rs
+++ rust/src/day04/mod.rs
@@ -1,63 +1,43 @@
+use memchr::Memchr2;
+
+use crate::utils::ByteSliceExt;
+
 #[inline]
-pub fn input() -> &'static [u8] {
-    static INPUT: &str = include_str!("input.txt");
-    INPUT.as_bytes()
-}
-
-pub fn part1(s: &[u8]) -> u16 {
-    let (mut prev, mut valid_passports, mut fields_present) = (0, 0, 0);
-    for pos in memchr::Memchr2::new(b'\n', b' ', s) {
-        if prev == pos {
-            valid_passports += (fields_present == 7) as u16;
-            fields_present = 0;
-        } else {
-            fields_present += (unsafe { *s.get_unchecked(prev) } != b'c') as u8;
-        }
-        prev = pos + 1;
-    }
-    valid_passports
-}
-
 fn check_year(s: &[u8], lo: u16, hi: u16) -> bool {
-    // yes, we could use `u16::from_str()`... but this is faster
     s.len() == 4 && {
-        let c0 = unsafe { *s.get_unchecked(0) };
-        let c1 = unsafe { *s.get_unchecked(1) };
-        let c2 = unsafe { *s.get_unchecked(2) };
-        let c3 = unsafe { *s.get_unchecked(3) };
-        (c0.is_ascii_digit() && c1.is_ascii_digit() && c1.is_ascii_digit() && c2.is_ascii_digit())
-            && {
-                let v = ((c0 - b'0') as u16) * 1000
-                    + ((c1 - b'0') as u16) * 100
-                    + ((c2 - b'0') as u16) * 10
-                    + ((c3 - b'0') as u16);
-                v >= lo && v <= hi
-            }
+        let d0 = s.get_digit_at(0);
+        let d1 = s.get_digit_at(1);
+        let d2 = s.get_digit_at(2);
+        let d3 = s.get_digit_at(3);
+        (d0 < 10 && d1 < 10 && d2 < 10 && d3 < 10) && {
+            let v = (d0 as u16) * 1000 + (d1 as u16) * 100 + (d2 as u16) * 10 + (d3 as u16);
+            v >= lo && v <= hi
+        }
     }
 }
 
+#[inline]
 fn check_height(s: &[u8], lo: u16, hi: u16) -> bool {
-    // yes, we could use `u16::from_str()`... but this is faster
     s.len() >= 2 && {
-        let c0 = unsafe { *s.get_unchecked(0) };
-        let c1 = unsafe { *s.get_unchecked(1) };
-        (c0.is_ascii_digit() && c1.is_ascii_digit()) && {
-            let v = ((c0 - b'0') as u16) * 10 + ((c1 - b'0') as u16);
-            match s.len() {
+        let d0 = s.get_digit_at(0);
+        let d1 = s.get_digit_at(1);
+        let v = (d0 as u16) * 10 + (d1 as u16);
+        (d0 < 10 && d1 < 10)
+            && match s.len() {
+                2 => v >= lo && v <= hi,
                 3 => {
-                    let c2 = unsafe { *s.get_unchecked(2) };
-                    c2.is_ascii_digit() && {
-                        let v = v * 10 + ((c2 - b'0') as u16);
+                    let d2 = s.get_digit_at(2);
+                    (d2 < 10) && {
+                        let v = v * 10 + (d2 as u16);
                         v >= lo && v <= hi
                     }
                 }
-                2 => v >= lo && v <= hi,
                 _ => false,
             }
-        }
     }
 }
 
+#[inline]
 fn is_valid(k: &[u8], v: &[u8]) -> bool {
     match k {
         b"byr" => check_year(v, 1920, 2002),
@@ -71,23 +51,40 @@ fn is_valid(k: &[u8], v: &[u8]) -> bool {
                 _ => false,
             }
         }
-        b"hcl" => {
-            v.len() == 7
-                && unsafe { *v.get_unchecked(0) == b'#' }
-                && v[1..].iter().all(u8::is_ascii_hexdigit)
-        }
-        b"ecl" => match v {
-            b"amb" | b"blu" | b"brn" | b"gry" | b"grn" | b"hzl" | b"oth" => true,
-            _ => false,
-        },
+        b"hcl" => v.len() == 7 && v.get_first() == b'#' && v[1..].iter().all(u8::is_ascii_hexdigit),
+        b"ecl" => matches!(
+            v,
+            b"amb" | b"blu" | b"brn" | b"gry" | b"grn" | b"hzl" | b"oth"
+        ),
         b"pid" => v.len() == 9 && v.iter().all(u8::is_ascii_digit),
         _ => false,
     }
 }
 
+#[inline]
+pub fn input() -> &'static [u8] {
+    include_bytes!("input.txt")
+}
+
+#[inline]
+pub fn part1(s: &[u8]) -> u16 {
+    let (mut prev, mut valid_passports, mut fields_present) = (0, 0, 0);
+    for pos in Memchr2::new(b'\n', b' ', s) {
+        if prev == pos {
+            valid_passports += (fields_present == 7) as u16;
+            fields_present = 0;
+        } else {
+            fields_present += (s.get_at(prev) != b'c') as u8;
+        }
+        prev = pos + 1;
+    }
+    valid_passports
+}
+
+#[inline]
 pub fn part2(s: &[u8]) -> u16 {
     let (mut prev, mut valid_passports, mut fields_valid) = (0, 0, 0);
-    for pos in memchr::Memchr2::new(b'\n', b' ', s) {
+    for pos in Memchr2::new(b'\n', b' ', s) {
         if prev == pos {
             valid_passports += (fields_valid == 7) as u16;
             fields_valid = 0;
