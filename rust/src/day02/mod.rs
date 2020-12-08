5619e8ab7dddbe41e0141ee379ec3c60a95680de
Ivan Smirnov <i.s.smirnov@gmail.com>
2020-12-08 19:54:40 +0000

    Day 02: Rust: speed it up, make it superfast

--- rust/src/day02/mod.rs
+++ rust/src/day02/mod.rs
@@ -1,6 +1,7 @@
-use arrayvec::ArrayVec;
+use std::hint::unreachable_unchecked;
+use std::slice::from_raw_parts;
 
-use crate::parse::{expect_char, expect_str, parse_char, parse_uint, parse_until_eol};
+use memchr::memchr;
 
 #[derive(Clone, Debug, Default)]
 pub struct Password<'a> {
@@ -10,55 +11,66 @@ pub struct Password<'a> {
     pub char: u8,
 }
 
+#[inline]
+unsafe fn parse_u8_superfast(s: &mut *const u8) -> u8 {
+    let d0 = (**s).wrapping_sub(b'0');
+    *s = s.add(1);
+    let d1 = (**s).wrapping_sub(b'0');
+    if d1 < 10 {
+        *s = s.add(1);
+        d0 * 10 + d1
+    } else {
+        d0
+    }
+}
+
 impl<'a> Password<'a> {
-    pub fn parse(s: &'a [u8]) -> Option<Self> {
-        let (s, n1) = parse_uint(s)?;
-        let (s, _) = expect_char(b'-')(s)?;
-        let (s, n2) = parse_uint(s)?;
-        let (s, _) = expect_char(b' ')(s)?;
-        let (s, char) = parse_char(s)?;
-        let (pass, _) = expect_str(b": ")(s)?;
-        Some(Self { pass, n1, n2, char })
-    }
-
-    pub fn parse_all(s: &'a str) -> Option<ArrayVec<[Self; 1024]>> {
-        let mut s = s.as_bytes();
-        let mut vec = ArrayVec::<[Self; 1024]>::default();
-        while !s.is_empty() {
-            let res = parse_until_eol(s)?;
-            if res.1.is_empty() {
-                break;
-            }
-            unsafe { vec.push_unchecked(Self::parse(res.1)?) };
-            s = res.0;
-        }
-        Some(vec)
+    #[inline]
+    pub unsafe fn parse_superfast(s: &mut &'a [u8]) -> Self {
+        let mut p = s.as_ptr();
+        let n1 = parse_u8_superfast(&mut p);
+        p = p.add(1);
+        let n2 = parse_u8_superfast(&mut p);
+        p = p.add(1);
+        let char = *p;
+        p = p.add(3);
+        let i = memchr(b'\n', from_raw_parts(p, 32)).unwrap_or_else(|| unreachable_unchecked());
+        let pass = from_raw_parts(p, i);
+        p = p.add(i + 1);
+        *s = from_raw_parts(p, s.len() - (p.offset_from(s.as_ptr()) as usize));
+        Self { pass, n1, n2, char }
     }
 }
 
-pub fn input() -> ArrayVec<[Password<'static>; 1024]> {
-    static INPUT: &str = include_str!("input.txt");
-    Password::parse_all(INPUT).unwrap()
+pub fn input() -> &'static [u8] {
+    static INPUT: &[u8] = include_bytes!("input.txt");
+    INPUT
 }
 
-pub fn part1(s: &[Password]) -> u16 {
+#[inline]
+pub fn part1(mut s: &[u8]) -> u16 {
     let mut valid = 0u16;
-    for p in s {
-        let mut n = 0u8;
-        for &c in p.pass.iter() {
-            n += (c == p.char) as u8;
-        }
-        valid += (n >= p.n1 && n <= p.n2) as u16;
+    while s.len() > 1 {
+        let p = unsafe { Password::parse_superfast(&mut s) };
+        let n = p
+            .pass
+            .into_iter()
+            .take(32)
+            .map(|&c| (c == p.char) as u8)
+            .sum::<u8>();
+        valid = valid.wrapping_add((n >= p.n1 && n <= p.n2) as u16);
     }
     valid
 }
 
-pub fn part2(s: &[Password]) -> u16 {
-    s.iter()
-        .map(|p| {
-            let match1 = unsafe { *p.pass.get_unchecked((p.n1 - 1) as usize) == p.char };
-            let match2 = unsafe { *p.pass.get_unchecked((p.n2 - 1) as usize) == p.char };
-            (match1 != match2) as u16
-        })
-        .sum()
+#[inline]
+pub fn part2(mut s: &[u8]) -> u16 {
+    let mut valid = 0;
+    while s.len() > 1 {
+        let p = unsafe { Password::parse_superfast(&mut s) };
+        let match1 = unsafe { *p.pass.get_unchecked((p.n1 - 1) as usize) == p.char };
+        let match2 = unsafe { *p.pass.get_unchecked((p.n2 - 1) as usize) == p.char };
+        valid += (match1 != match2) as u16;
+    }
+    valid
 }
